c11dd0142eba7abbf538a3aeb3e542896da8e39c
kameniarthur <kameniarthur138@gmail.com>
2026-01-14 10:41:59 +0100

    Refactor code structure for improved readability and maintainability; optimize performance in key functions.

--- services/index.js
+++
@@ -1,9 +0,0 @@
-const emailService = require('./emailService');
-const fileService = require('./fileService');
-const paymentService = require('./paymentService');
-
-module.exports = {
-    emailService,
-    fileService,
-    paymentService
-};
